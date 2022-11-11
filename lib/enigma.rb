8d3e0451835a6d9d52f51abeb9a753947895e9c5
Joe King <j.king92@gmail.com>
2022-11-11 08:45:28 -0700

    feat: add #key_to_initial_offset and fix typo in associated test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,4 +5,9 @@ class Enigma
     5.times { key << rand(10) }
     key
   end
+
+  def key_to_initial_offset(key)
+    consecutive_key = key.each_cons(2).to_a
+    consecutive_key.map { |pair| pair.join.to_i }
+  end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Enigma do
   describe '#key_to_initial_offset()' do
     it 'turns the 5 random numbers into array of the offsets for ABCD' do
       expect(enigma.key_to_initial_offset([2, 6, 2, 3, 8])).to eq([26, 62, 23, 38])
-      expect(enigma.ket_to_initial_offset([5, 2, 4, 9, 7])).to eq([52, 24, 49, 97])
+      expect(enigma.key_to_initial_offset([5, 2, 4, 9, 7])).to eq([52, 24, 49, 97])
     end
   end
 end
