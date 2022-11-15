c51d2b86649148f1d543fb3ea1715814da75bf9f
Joe King <j.king92@gmail.com>
2022-11-15 13:12:43 -0700

    refactor: Encrypt.rb so that it can take an optional date to use for encryption

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 require './lib/enigma'
+require './lib/key'
 
 message_file = File.open(ARGV[0], 'r')
 message = message_file.read
 message_file.close
 enigma = Enigma.new
-encryption = enigma.encrypt(message)
+date = ARGV[2] || enigma.today
+encryption = enigma.encrypt(message, Key.new, date)
 ciphertext = File.open(ARGV[1], 'w')
 ciphertext.write(encryption[:encryption])
 ciphertext.close
